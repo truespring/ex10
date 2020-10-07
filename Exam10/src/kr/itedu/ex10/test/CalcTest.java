d398b9af86f486d710bd1e747138e5e1937e88c6
truespring <true_spring_@naver.com>
2020-10-07 10:51:01 +0900

    Calc 오류 제거

--- Exam10/src/kr/itedu/ex10/test/CalcTest.java
+++ Exam10/src/kr/itedu/ex10/test/CalcTest.java
@@ -9,12 +9,24 @@ class CalcTest {
 
 	@Test
 	void test() {
-		int result = Calc.sum(1, 2);
-		Assert.assertEquals(3, result);
-		result = Calc.sum(10, 20);
-		Assert.assertEquals(30, result);
-		result = Calc.sum(11, 22);
-		Assert.assertEquals(33, result);
+//		int result = Calc.sum(1, 2);
+//		Assert.assertEquals(3, result);
+//		result = Calc.sum(10, 20);
+//		Assert.assertEquals(30, result);
+//		result = Calc.sum(11, 22);
+//		Assert.assertEquals(33, result);
+		
+		int n1 = 10;
+		int n2 = 20;
+		Assert.assertEquals(n1 + n2, Calc.sum(n1, n2));
+		
+		n1 = 13;
+		n2 = 99;
+		Assert.assertEquals(n1 + n2, Calc.sum(n1, n2));
+		
+		n1 = 1;
+		n2 = 1;
+		Assert.assertEquals(n1 + n2, Calc.sum(n1, n2));
 	}
 	
 
